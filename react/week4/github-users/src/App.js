cbc744ac73f073f8e3cb6f52323aea834102f5ce
mgiota <panagiota.mitsopoulou@elastic.co>
2021-09-27 23:48:54 +0200

    use axios authentication

--- react/week4/github-users/src/App.js
+++ react/week4/github-users/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 function App() {
@@ -6,9 +7,21 @@ function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch(`https://api.github.com/search/users?q=${username}`)
-      .then(res => res.json())
-      .then(users => setUsers(users.items));
+    // fetch(`{"message":"API rate limit exceeded for 95.91.253.143. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)","documentation_url":"https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting"}
+    // `)
+    // // fetch(`https://api.github.com/search/users?q=${username}`)
+    //   .then(res => res.json())
+    //   .then();
+    axios.get(`https://api.github.com/search/users?q=${username}`, {
+      auth: {
+        username: '73f8a8e1563a4a2f9fc5',
+        password: '3b91407a3a7df82d567b235501432e978c8c72bd'
+      }
+    })
+    .then(users => {
+      console.log(users, '!!users')
+      setUsers(users.data.items)
+    })
   })
 
   return (
